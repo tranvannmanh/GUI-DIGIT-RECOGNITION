8b6c784863cb9e72d4b4bad81ec2b9a81a0d7db9
Tran Van Manh <manh.tranvann@gmail.com>
2022-12-08 23:25:33 +0700

    apply model

--- GUI.py
+++ GUI.py
@@ -5,20 +5,23 @@ import win32gui
 from PIL import ImageGrab, Image
 import numpy as np
 
-# model = load_model('mnist.h5')
+model = load_model('mnist.h5')
+
 
 def predict_digit(img):
-    #resize image to 28x28 pixels
-    img = img.resize((28,28))
-    #convert rgb to grayscale
+    # resize image to 28x28 pixels
+    img = img.resize((28, 28))
+    # convert rgb to grayscale
     img = img.convert('L')
     img = np.array(img)
-    #reshaping to support our model input and normalizing
-    img = img.reshape(1,28,28,1)
+    # reshaping to support our model input and normalizing
+    img = img.reshape(1, 28, 28, 1)
     img = img/255.0
-    #predicting the class
-    # res = model.predict([img])[0]
-    # return np.argmax(res), max(res)
+    # predicting the class
+    res = model.predict([img])[0]
+    # print('{} / {} - {}'.format(res, np.argmax(res), max(res)))
+    return np.argmax(res), min(res)
+
 
 class App(tk.Tk):
     def __init__(self):
@@ -27,14 +30,17 @@ class App(tk.Tk):
         self.x = self.y = 0
 
         # Creating elements
-        self.canvas = tk.Canvas(self, width=300, height=300, bg = "white", cursor="cross")
+        self.canvas = tk.Canvas(
+            self, width=300, height=300, bg="white", cursor="cross")
         self.label = tk.Label(self, text="Thinking..", font=("Helvetica", 48))
-        self.classify_btn = tk.Button(self, text = "Recognise", command = self.classify_handwriting) 
-        self.button_clear = tk.Button(self, text = "Clear", command = self.clear_all)
+        self.classify_btn = tk.Button(
+            self, text="Recognise", command=self.classify_handwriting)
+        self.button_clear = tk.Button(
+            self, text="Clear", command=self.clear_all)
 
         # Grid structure
         self.canvas.grid(row=0, column=0, pady=2, sticky=W, )
-        self.label.grid(row=0, column=1,pady=2, padx=2)
+        self.label.grid(row=0, column=1, pady=2, padx=2)
         self.classify_btn.grid(row=1, column=1, pady=2, padx=2)
         self.button_clear.grid(row=1, column=0, pady=2)
 
@@ -45,18 +51,20 @@ class App(tk.Tk):
         self.canvas.delete("all")
 
     def classify_handwriting(self):
-        HWND = self.canvas.winfo_id() # get the handle of the canvas
-        rect = win32gui.GetWindowRect(HWND) # get the coordinate of the canvas
+        HWND = self.canvas.winfo_id()  # get the handle of the canvas
+        rect = win32gui.GetWindowRect(HWND)  # get the coordinate of the canvas
         im = ImageGrab.grab(rect)
 
         digit, acc = predict_digit(im)
-        self.label.configure(text= str(digit)+' - '+ str(int(acc*100))+'%')
+        self.label.configure(text=str(digit)+' - ' + str(int(acc*100))+'%')
 
     def draw_lines(self, event):
         self.x = event.x
         self.y = event.y
-        r=8
-        self.canvas.create_oval(self.x-r, self.y-r, self.x + r, self.y + r, fill='black')
+        r = 8
+        self.canvas.create_oval(
+            self.x-r, self.y-r, self.x + r, self.y + r, fill='black')
+
 
 app = App()
 mainloop()
